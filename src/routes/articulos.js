d0e2c340e0f936829f4750bfc6f6a73bab9d938f
Rodrigo <rmg.jp@outlook.com>
2021-03-08 12:21:01 -0600

    Arreglado el ruteo y pantalla de nuevo articulo

--- src/routes/articulos.js
+++ src/routes/articulos.js
@@ -1,49 +1,43 @@
 const express = require('express');
 const router = express.Router();
-const usuario = require('../models/Articulo');
+const articulo = require('../models/Articulo');
 
 router.get('/articulos/view-articulo', ((req, res) =>
     res.render('articulos/view-articulo')))
 
-router.get('/articulos/articulo-guardado', ((req, res) =>
-    res.render('articulos/articulo-guardado')))
 
 router.get('/articulos/new-articulo', ((req, res) =>
     res.render('articulos/new-articulo')))
 
-router.post('/articulos/ArticuloGuardado', async (req, res) =>
-    {
-        const {nombre,marca,descripcion,precio} = req.body;
-        const errors = [];
-        if(!nombre){
-            errors.push({text: 'Por favor escribe un nombre'});
-        }
-        if(!marca){
-            errors.push({text: 'Por favor escribe una marca'});
-        }
-        if(!descripcion){
-            errors.push({text: 'Por favor escribe una descripcion'});
-        }
-        if(!precio){
-            errors.push({text: 'Por favor escribe un precio'});
-        }
-
-        if(errors.length > 0){
-            res.render('/articulos/new-articulo',{
-                errors,
-                nombre,
-                marca,
-                descripcion,
-                precio
-            });
-        }
-        else{
-            const newArticulos = new usuario({nombre,descripcion,marca, precio});
-            await newArticulos.save();
-            console.log(newUsuario);
-            res.redirect('/articulos/articulo-guardado');
-        }
+router.post('/articulos/articulo-guardado', async (req,res)=>{
+    const {nombre, precio, marca, descripcion} = req.body;
+    const errors = [];
+    if(!nombre){
+        errors.push({text: 'Por favor escribe un nombre'});
     }
-);
-
+    if(!precio){
+        errors.push({text: 'Por favor escribe un precio'});
+    }
+    if(!marca){
+        errors.push({text: 'Por favor escribe una marca'});
+    }
+    if(!descripcion){
+        errors.push({text: 'Por favor escribe una descripcion'});
+    }
+    if(errors.length > 0){
+        res.render('articulos/new-articulo',{
+            errors,
+            nombre,
+            precio,
+            marca,
+            descripcion
+        });
+    }
+    else{
+        const newArticulo = new articulo({nombre, descripcion, marca, precio});
+        await newArticulo.save();
+        console.log(newArticulo);
+        res.redirect('/');
+    }
+});
 module.exports = router;
